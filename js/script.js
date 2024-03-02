3181af6a2fbdadff75bfb491b192b4ff0b6d6e15
Antonio Llamas <antoniollamasln@gmail.com>
2024-03-02 14:09:11 +0100

    add

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const button = document.getElementById('get-pokemon')
+
+const select = document.getElementById('pokemon-select')
+
+const pokemon = document.querySelector('.pokemon')
+
+const baseURL = 'https://pokeapi.co/api/v2/pokemon/'
+
+button.addEventListener('click', function () {
+	const pokemon = select.value
+
+	getPokemon(pokemon)
+})
+
+function getPokemon(pokemonName) {
+	const endpoint = baseURL + pokemonName
+
+	fetch(endpoint)
+		.then((res) => res.json())
+		.then((data) => {
+			const pokemon = {
+				name: data.name,
+				image: data.sprites.front_default,
+				types: data.types.map((typeObj) => typeObj.type.name),
+				weight: data.weight,
+				height: 7,
+			}
+			renderPokemon(pokemon)
+		})
+		.catch((err) => alert(`Error al encontrar el pokemon "${pokemonName}"`))
+}
+
+function renderPokemon(pokemonDetails) {
+	console.log(pokemonDetails)
+	pokemon.innerHTML = ''
+
+	pokemon.innerHTML += `
+    <h2 class="pokemon__title">${pokemonDetails.name}</h2>
+
+		<ul class="pokemon__types">
+			${pokemonDetails.types
+				.map((type) => `<li class="pokemon__type ${type}">${type}</li>`)
+				.join('')}	
+		</ul>
+
+    <img class="pokemon__image" src="${
+			pokemonDetails.image
+		}" alt="pokemonDetails.name}" />
+    <ul class="pokemon__details">
+      <li><b>Peso:</b> ${pokemonDetails.weight}</li>
+      <li><b>Altura:</b> ${pokemonDetails.height}</li>
+    </ul>
+  `
+}
